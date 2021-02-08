52ecbda8800885d85676c06e0ca49063b86c5379
Kevin Bowman <kbowman@kauffman.org>
2021-02-08 12:33:02 -0600

    working except for
     single-quote escaping

--- import.sh
+++ import.sh
@@ -25,21 +25,50 @@ cat $FILE_LIST \
 	| sed -e 's/Private Firm Documents/Clients/' \
 	| sed -e 's/_/\//g' \
 	| awk '{printf "mkdir -p \"%s\"\n", $0}' \
-	> makedirs.sh
+	> $WORKING_FOLDER/makedirs.sh
+
+
+# We have to deal with bad characters in file names
+#   the export process uses underscores "_" as
+#     folder delineation, so we'll have to remove
+#     those from any file names
+#   the team uses dollar signs "$" in filenames
+#     we'll plan to single-quote the file paths
+#     to avoid expansion of special characters
+#   except, this creates a problem for 
+#     single-quote in filenames, so, we'll have to 
+#     escape those?  replace those?
 
 # remove underscores (_) from file name
 #  replace with dash (-)
 #  this prevents us from losing our minds when
 #  we expand the file names into folder structure
-# remove the folders so we only have files left
-# build the command that moves files into appropriate 
-#   place in the structure
-#   start with the place
+cat $FILE_LIST \
+	| awk 'BEGIN{FS="/"}
+	{if($3 ~ /_/) {
+		printf "mv '\''./files/%s'\'' '\''./files/%s/%s/", $0,$1,$2
+		gsub(/_/,"-",$3)
+		printf "%s'\''\n",$3
+	}
+	else {next}
+	}' > $WORKING_FOLDER/removeunderscores.sh
+
+bash $WORKING_FOLDER/removeunderscores.sh
+
+# rebuild file list since we changed
+#   the names of some of our files
+cd $WORKING_FOLDER/files && find . > $FILE_LIST
+
+# build command to move everything to new location
+#   first, remove lines that don't contain any files
+#     we have already used these linese above
+#     to make the folders
+#   then, build the command that moves files into appropriate 
+#     place in the structure - replacing the _ with /
 awk -F'/' 'NF!=2' $FILE_LIST \
-	| awk '{gsub(/_/,"-",$NF)} 1' \
 	| awk 'BEGIN{FS="_"} 
 	{printf "mv '\''./files/%s'\'' '\''./Clients/", $0}
-	{ for (i=2; i<=NF-1; i++) {
+	{ for (i=2; i<=NF; i++) {
 		printf "%s/", $i}
 	}
-	{printf "%s'\''\n",$NF}' > movefiles.sh
+	{printf '\''\n"}' > $WORKING_FOLDER/movefiles.sh
